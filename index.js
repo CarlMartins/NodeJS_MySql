3d088b8111af66f5f9f54baff4a7586e9ee36e8c
Carlos Martins <lemos_oliveiraa@hotmail.com>
2020-12-30 11:43:29 -0300

    Instalacao Sequelize e conexao com banco de dados MySQL

--- index.js
+++ index.js
@@ -3,9 +3,26 @@ const app = express();
 const handlebars = require('express-handlebars');
 
 // Config
-// Template engine
+//// Template engine
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+//// Conexão MySQL
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize('postagensdb', 'root', '1234', {
+    host: 'localhost',
+    dialect: 'mysql'
+});
+
+sequelize.authenticate()
+    .then(() => 
+    {
+        console.log("Conectado com sucesso ao banco de dados.");
+    })
+    .catch((erro) =>
+    {
+        console.log("Falha na conexão com o banco de dados");
+    });
 
 app.listen(3000, () =>
 {
