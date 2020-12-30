312bab2a1303dc9e94a2e33073b0febb28ce9dcb
Carlos Martins <lemos_oliveiraa@hotmail.com>
2020-12-30 11:30:32 -0300

    Instalacao Handlebars e primeiras configuracoes

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Ola mundo");
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+
+// Config
+// Template engine
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.listen(3000, () =>
+{
+    console.log("Servidor rodando na URL: http://localhost:3000");
+});
+
